ad35f9db9b003322674292210314b07c340a8aaa
dilan-wil <heavenguard237@gmail.com>
2025-09-11 03:37:51 +0000

    change pusher

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,23 +1,25 @@
-import type React from "react"
-import { Inter } from "next/font/google"
-import { ThemeProvider } from "@/components/theme-provider"
-import { AuthProvider } from "@/contexts/auth-context"
-import { Toaster } from "@/components/ui/toaster"
-import ClientLayout from "./client-layout"
-import ProtectedRoute from "@/components/protected-route"
+import type React from "react";
+import { Inter } from "next/font/google";
+// import { ThemeProvider } from "@/components/theme-provider"
+// import { AuthProvider } from "@/contexts/auth-context"
+// import { Toaster } from "@/components/ui/toaster"
+import ClientLayout from "./client-layout";
+import ProtectedRoute from "@/components/protected-route";
 
-
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 interface RootLayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
-export default function DashboardLayout({ children }: {children : React.ReactNode}) {
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <ProtectedRoute>
       <ClientLayout>{children}</ClientLayout>
     </ProtectedRoute>
-  )
+  );
 }
-
